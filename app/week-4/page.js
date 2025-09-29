d23abafe2e037995725545afc352de54613ebabf
xder-08 <xander.rancap@edu.sait.ca>
2025-09-29 15:23:08 -0600

    fixed the forgotten decrement function(oopsies) and logic!!!

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -8,4 +8,38 @@ export default function NewItem() {
   const increment = () => {
     setQuantity((prev) => (prev < 20 ? prev + 1 : prev));
   };
+
+  const decrement = () => {
+    setQuantity((prev) => (prev > 1 ? prev - 1 : prev));
+  };
+
+  return (
+    <div className="flex flex-col items-center p-6 bg-white rouded-2xl shadow-md w-72">
+      <h2 className="text-xl font-semibold mb-4">Select Quantity</h2>
+
+      <div className="flex items-center gap-4">
+        <button
+          onClick={decrement}
+          disabled={quantity === 1}
+          className={`px-4 py-2 rounded-lg font-bold ${
+            quantity === 1
+          }`}
+        >
+          -
+        </button>
+
+        <span className="text-2xl font-bold">{quantity}</span>
+
+        <button
+          onClick={increment}
+          disabled={quantity === 20}
+          className={`px-4 py-2 rounded-lg font-bold ${
+            quantity === 20
+          }`}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
