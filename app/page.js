4dc746b50a6d9bda95bb10e61d99cdafa42b7536
xder-08 <xander.rancap@edu.sait.ca>
2025-10-14 15:34:17 -0600

    kinda done

--- app/page.js
+++ app/page.js
@@ -3,8 +3,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
+      <h1
+        className="text-xl font-black mb-4 text-cyan-400 uppercase tracking-widest"
+        style={{
+          textShadow:
+            "2px 2px 0px #000, -1px -1px 0px #000, 1px -1px 0px #000, -1px 1px 0px #000",
+        }}
+      >
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <p className="text-l font-black mb-4 text-fuchsia-400 border border-gray-300 rounded px-4 py-2 w-140 mb-4">
         <Link href="/week-2">Go to Week 2</Link>
         <Link href="/week-3"> - Go to Week 3</Link>
         <Link href="/week-4"> - Go to Week 4</Link>
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -37,11 +37,20 @@ export default function Field() {
     <form onSubmit={handleSubmit}>
       <div className="flex items-center justify-center min-h-screen bg-blue-600">
         <div className="flex flex-col items-center p-6 bg-gradient-to-br from-purple-400 via-pink-300 to-blue-400 rounded-none shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] w-72 border-4 border-black">
+          <h2
+            className="text-xl font-black mb-4 text-cyan-400 uppercase tracking-widest"
+            style={{
+              textShadow:
+                "2px 2px 0px #000, -1px -1px 0px #000, 1px -1px 0px #000, -1px 1px 0px #000",
+            }}
+          >
+            Item Name
+          </h2>
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            placeholder="Item name"
+            placeholder="enter item name"
             required
             className="border border-gray-300 rounded px-4 py-2 w-64 mb-4"
           />
@@ -102,7 +111,7 @@ export default function Field() {
               <option value="dry goods">Dry Goods</option>
               <option value="beverages">Beverages</option>
               <option value="snacks">Snacks</option>
-              <option value="household">Household</option>
+              <option value="household">Household</option>  
               <option value="other">Other</option>
             </select>
           </div>
